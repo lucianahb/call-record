e13421d5605c2e1272094d441bc9c1c55555e64d
luciheit@gmail.com <luciheit@gmail.com>
2021-03-30 14:14:40 -0300

    Create some validates

--- calls/serializers.py
+++ calls/serializers.py
@@ -4,6 +4,14 @@ from rest_framework import serializers
 
 class CallRecordSerializer(serializers.ModelSerializer):
 
+    def validate_source(self, data):
+        if not data.isdigit():
+            raise serializers.ValidationError('Hey, put a phone number, please!')
+
+    def validate_destination(self, data):
+        if not data.isdigit():
+            raise serializers.ValidationError('Hey, put a phone number, please!')
+
     class Meta:
         model = CallRecord
         fields = (
