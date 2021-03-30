2ec3c6974d66fb08837a5cc0820ebca4ccbc6c10
luciheit@gmail.com <luciheit@gmail.com>
2021-03-30 11:50:29 -0300

    Create Bill router

--- calls/urls.py
+++ calls/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import CallRecordViewSet
+from .views import CallRecordViewSet, BillViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'Call-Record', CallRecordViewSet, basename="CallRecord")
+router.register(r'Bill', BillViewSet, basename="Bill")
 
 
 urlpatterns = [
